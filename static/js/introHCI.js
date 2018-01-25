dda10d77884134bd4da7aac19c924b32114731e5
Vivian Ta <vivian.vta@gmail.com>
2018-01-25 00:34:42 -0800

    updated js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,36 @@ $(document).ready(function() {
 	initializePage();
 })
 
+$("a.thumbnail").click(projectClick);
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+        $('.jumbotron h1').toggleClass('h2');
+		$('.jumbotron h1').text("Made you Click 😂");
+        $("#testjs").text("It works!");
+        $(".jumbotron p").addClass("active");
+        $("#testjs").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+        $(containingProject).fadeOut();
+        $(containingProject).fadeOut("slow");
+//       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
